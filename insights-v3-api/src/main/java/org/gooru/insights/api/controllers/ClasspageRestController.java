7876790c8d9061c62cfc6eb156a3a81b6832287c
daniel <arokiadaniel.a@goorulearning.org>
2015-06-04 20:09:34 +0530

    New packages and Json deserializer added

--- insights-v3-api/src/main/java/org/gooru/insights/api/controllers/ClasspageRestController.java
+++ insights-v3-api/src/main/java/org/gooru/insights/api/controllers/ClasspageRestController.java
@@ -7,18 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.gooru.insights.api.constants.InsightsOperationConstants;
+import org.gooru.insights.api.models.RequestParamDTO;
 import org.gooru.insights.api.security.AuthorizeOperations;
+import org.gooru.insights.api.serializer.JsonDeserializer;
 import org.gooru.insights.api.services.ClasspageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value="classpage/")
+@RequestMapping(value="collection/")
 public class ClasspageRestController extends BaseController
 {
 	@Autowired
@@ -35,4 +38,19 @@ public class ClasspageRestController extends BaseController
 		return getModel(m);
 	}
    
+	@RequestMapping(value="/{collectionGooruId}/sessions",method ={ RequestMethod.GET,RequestMethod.POST})
+	@AuthorizeOperations(operations =  InsightsOperationConstants.OPERATION_INSIHGHTS_CLASS_OE_VIEW)
+	@ResponseBody
+	public ModelAndView getSessions(HttpServletRequest request,@RequestBody String data,@PathVariable(value="collectionGooruId") String collectionGooruId,HttpServletResponse response) throws Exception{
+		Map<String, Object> m = new HashMap<String, Object>();
+		RequestParamDTO requestParam = buildSessionActivityFromInputParameters(data);
+		m.put("collectionGooruId", collectionGooruId);
+		m.put("classGooruId", requestParam.getClassGooruId());
+		m.put("reportType", requestParam.getReportType());
+		return getModel(m);
+	}
+	
+	private RequestParamDTO buildSessionActivityFromInputParameters(String data) {
+		return JsonDeserializer.deserialize(data, RequestParamDTO.class);
+	}
 }
---
+++ insights-v3-api/src/main/java/org/gooru/insights/api/repository/ClassRepositoryImpl.java
@@ -0,0 +1,5 @@
+package org.gooru.insights.api.repository;
+
+public class ClassRepositoryImpl implements ClassRepository {
+
+}
