f159116cbbce4c7a5d966d539d5902c08af32fc6
daniel <arokiadaniel.a@goorulearning.org>
2015-08-18 15:28:11 +0530

    Data security authentication

--- insights-v1-api/src/main/java/org/gooru/insights/api/security/MethodAuthorizationAspect.java
+++ insights-v1-api/src/main/java/org/gooru/insights/api/security/MethodAuthorizationAspect.java
@@ -32,11 +32,13 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.gooru.insights.api.constants.ApiConstants;
+import org.gooru.insights.api.models.InsightsConstant.ColumnFamily;
 import org.gooru.insights.api.models.User;
 import org.gooru.insights.api.services.CassandraService;
 import org.gooru.insights.api.services.RedisService;
@@ -54,6 +56,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.netflix.astyanax.connectionpool.OperationResult;
 import com.netflix.astyanax.model.ColumnList;
 
 
@@ -157,7 +160,7 @@ public class MethodAuthorizationAspect extends OperationAuthorizer {
 					validStatus.put(PROCEED, true);
 					return validStatus;
 				}
-				if (hasAuthority(authorizeOperations, jsonObject)) {
+				if (hasAuthority(authorizeOperations, jsonObject,request)) {
 					session.setAttribute(ApiConstants.SESSION_TOKEN,
 							sessionToken);
 					validStatus.put(PROCEED, true);
@@ -217,7 +220,7 @@ public class MethodAuthorizationAspect extends OperationAuthorizer {
 					user.setEmailId(jsonObj.getString(ApiConstants.EMAIL_ID));
 					user.setGooruUId(jsonObj.getString(ApiConstants.GOORU_U_ID));
 					if (hasGooruAdminAuthority(authorizeOperations, jsonObj)
-							|| hasAuthority(authorizeOperations, jsonObj)) {
+							|| hasAuthority(authorizeOperations, jsonObj,request)) {
 						session.setAttribute(ApiConstants.SESSION_TOKEN,
 								sessionToken);
 						return true;
@@ -252,7 +255,7 @@ public class MethodAuthorizationAspect extends OperationAuthorizer {
 		
 	}
 	
-	private boolean hasAuthority(AuthorizeOperations authorizeOperations,JSONObject jsonObj){
+	private boolean hasAuthority(AuthorizeOperations authorizeOperations,JSONObject jsonObj,HttpServletRequest request){
 			String userRole = null;
 			try {
 				userRole = jsonObj.getString(ApiConstants.USER_ROLE_SETSTRING);
@@ -266,10 +269,58 @@ public class MethodAuthorizationAspect extends OperationAuthorizer {
 				InsightsLogger.debug("role : " +roles + "roleAuthority > :"+userRole.contains(roles));
 				for(String role : roles.split(",")){
 					if((userRole.contains(role))){
-						return true;
+						if(operations.equalsIgnoreCase("Class~View")){
+							return isValidUser(jsonObj, request);
+						}else{
+							return true;
+						}
 					}
 				}
 			}
 		return false;
+	}
+	
+	private boolean isValidUser(JSONObject jsonObject, HttpServletRequest request) {
+		try {
+			String userUidFromSession = jsonObject.getString(ApiConstants.PARTY_UId);
+			String userIdFromRequest = RequestUtils.getUserIdFromRequestParam(request);
+			String classId = null;
+			String pathInfo = request.getPathInfo();
+			if (pathInfo.startsWith("/class/")) {
+				String[] pathParts = pathInfo.split("/");
+				classId = pathParts[2];
+			} else {
+				classId = RequestUtils.getClassIdFromRequestParam(request);
+			}
+			if (StringUtils.isBlank(userIdFromRequest) && "ANONYMOUS".equalsIgnoreCase(userUidFromSession)) {
+				InsightsLogger.info("ANONYMOUS user can't be a teacher class creator");
+				return false;
+			} else if (userIdFromRequest.equalsIgnoreCase(userUidFromSession)) {
+				InsightsLogger.info("Session Token and user uid that is available in url is mismatching..");
+				return true;
+			} else if (StringUtils.isNotBlank(classId) && userUidFromSession.equalsIgnoreCase(getTeacherUid(classId))) {
+				InsightsLogger.info("User is not a valid Class creatior or teacher..");
+				return true;
+			}
+
+		} catch (Exception e) {
+			InsightsLogger.error("Exception", e);
+		}
+		return false;
+	}
+
+	private String getTeacherUid(String classGooruId) {
+		String teacherUid = null;
+		if (StringUtils.isNotBlank(classGooruId)) {
+			try {
+				OperationResult<ColumnList<String>> classData = cassandraService.read(ColumnFamily.CLASS.getColumnFamily(), classGooruId);
+				if (!classData.getResult().isEmpty() && classData.getResult().size() > 0) {
+					teacherUid = classData.getResult().getStringValue(ApiConstants._CREATOR_UID, null);
+				}
+			} catch (Exception e) {
+				InsightsLogger.error("Exception", e);
+			}
+		}
+		return teacherUid;
 	}
 }
